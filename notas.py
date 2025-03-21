62014a97c08e2cbcf37d3f5b37c9be4353f1d372
santiagotrading <santiagohernandeztobon24@gmail.com>
2025-03-20 23:55:04 -0500

    login creado

--- notas.py
+++ notas.py
@@ -10,3 +10,13 @@ def register_user():
     print("Usuario registrado con éxito.\n")
 
 
+def login():
+    print("\n--- Login ---")
+    username = input("Usuario: ")
+    password = input("Contraseña: ")
+    for user in users:
+        if user["username"] == username and user["password"] == password:
+            print("Login exitoso\n")
+            return user
+    print("Usuario o contraseña incorrectos.\n")
+    return None
