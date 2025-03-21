87dff7274f865d590133da46babdb8ef058c36c2
santiagotrading <santiagohernandeztobon24@gmail.com>
2025-03-20 23:59:57 -0500

    funcionesnotas

--- notas.py
+++ notas.py
@@ -20,3 +20,21 @@ def login():
             return user
     print("Usuario o contraseña incorrectos.\n")
     return None
+
+def add_grade():
+    print("\n--- Agregar Nota ---")
+    student = input("Ingrese nombre del estudiante: ")
+    subject = input("Ingrese la materia: ")
+    grade = float(input("Ingrese la nota: "))
+    grades.append({"student": student, "subject": subject, "grade": grade})
+    print("Nota agregada con éxito.\n")
+
+def view_grades():
+    print("\n--- Ver Notas ---")
+    student = input("Ingrese su nombre para ver sus notas: ")
+    found_grades = [g for g in grades if g["student"] == student]
+    if found_grades:
+        for g in found_grades:
+            print(f"Materia: {g['subject']}, Nota: {g['grade']}")
+    else:
+        print("No se encontraron notas.\n")
